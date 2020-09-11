1f194bf460b7c5eaf015464a9828ac060feb011f
hiroomi ohori <tkgyb713@yahoo.co.jp>
2020-09-11 23:41:45 +0900

    databinding chip

--- app/src/main/java/com/example/demoapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/demoapp/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.demoapp.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,26 +11,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.demoapp.R;
+import com.example.demoapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
+    private String TAG = getClass().getSimpleName();
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+        FragmentHomeBinding binding = FragmentHomeBinding.inflate(inflater, (ViewGroup) root,false);
+        homeViewModel.getIsBarcodeEnable().observe(getViewLifecycleOwner(),b->{
+            //Changed
+            Log.d(TAG,b+"");
         });
-        return root;
+        binding.setViewModel(homeViewModel);
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/demoapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/demoapp/ui/home/HomeViewModel.java
@@ -1,19 +1,28 @@
 package com.example.demoapp.ui.home;
 
+import android.util.Log;
+import android.view.View;
+
+import androidx.databinding.Bindable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.material.chip.Chip;
+
 public class HomeViewModel extends ViewModel {
-
-    private MutableLiveData<String> mText;
-
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    private MutableLiveData<Boolean> isBarcodeEnable = new MutableLiveData<>();
+    private MutableLiveData<Boolean> is2DcodeEnable = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isOcrcodeEnable = new MutableLiveData<>();
+    public MutableLiveData<Boolean> getIsBarcodeEnable() {
+        return isBarcodeEnable;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public MutableLiveData<Boolean> getIs2DcodeEnable() {
+        return is2DcodeEnable;
+    }
+
+    public MutableLiveData<Boolean> getIsOcrcodeEnable() {
+        return isOcrcodeEnable;
     }
 }
